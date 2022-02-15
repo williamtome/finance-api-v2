7615f8d1db5c35fec4359126035f6f2458246b3e
William W. Tome <william.tomenh@gmail.com>
2022-02-14 23:40:10 -0300

    style(AuthController): remove request class unused.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use App\Traits\ApiResponser;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
