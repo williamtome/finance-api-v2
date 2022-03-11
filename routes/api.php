4d5457a63513be7631f58a700d4c95cb1004bd68
William <william.tomenh@gmail.com>
2022-03-10 22:07:26 -0300

    refactor(routes): remove login route.

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('auth')->group(function() {
-    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
 
