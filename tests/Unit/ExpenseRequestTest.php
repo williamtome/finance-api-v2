989f72d52333ad12155bb31a826b9dfa8330c800
William <william.tomenh@gmail.com>
2022-03-11 23:56:05 -0300

    test(ExpenseRequestTest): create test for ExpenseRequestTest class.

---
+++ tests/Unit/ExpenseRequestTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Requests\ExpenseRequest;
+use Illuminate\Support\Facades\Auth;
+use PHPUnit\Framework\TestCase;
+
+class ExpenseRequestTest extends TestCase
+{
+    private ExpenseRequest $request;
+
+    protected function setUp(): void
+    {
+        $this->request = new ExpenseRequest();
+
+        parent::setUp();
+    }
+
+    public function testShouldNotAuthorizeRequestIfTheUserIsNotLoggedIn()
+    {
+        Auth::shouldReceive('check')->once()->andReturnFalse();
+
+        $this->assertFalse($this->request->authorize());
+    }
+
+    public function testShouldAuthorizeRequestIfTheUserIsLoggedIn()
+    {
+        Auth::shouldReceive('check')->once()->andReturnTrue();
+
+        $this->assertTrue($this->request->authorize());
+    }
+
+    public function test_should_contain_all_the_expected_validation_rules()
+    {
+        $rules = [
+            'description' => 'required|string|max:191',
+            'amount' => 'required|numeric',
+            'date' => 'required|date',
+            'category' => [
+                'sometimes',
+                'filled',
+                'integer',
+                null
+            ],
+        ];
+
+        $this->assertEquals($rules, $this->request->rules());
+    }
+}
