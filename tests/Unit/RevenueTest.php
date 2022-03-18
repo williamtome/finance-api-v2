964336e0fad95c00078952ea9fbc5ebd430b3409
William <william.tomenh@gmail.com>
2022-03-18 17:21:38 -0300

    test(RevenueTest): refactoring tests.

--- tests/Unit/RevenueTest.php
+++ tests/Unit/RevenueTest.php
@@ -9,11 +9,14 @@ use Tests\TestCase;
 class RevenueTest extends TestCase
 {
     private array $header;
+    private User $user;
 
     protected function setUp(): void
     {
-        $this->header = ['Accept' => 'application/json'];
         parent::setUp();
+
+        $this->header = ['Accept' => 'application/json'];
+        $this->user = User::factory()->create();
     }
 
     public function test_should_forbid_an_unauthenticated_user_to_create_a_revenue()
@@ -24,54 +27,48 @@ class RevenueTest extends TestCase
             'date' => '2022-01-02'
         ];
 
-        $response = $this->json('POST','/api/revenue', $data, $this->header);
-
-        $response->assertUnauthorized();
+        $this->json('POST','/api/revenue', $data, $this->header)
+            ->assertUnauthorized();
     }
 
     public function test_should_allow_an_authenticated_user_to_create_a_revenue()
     {
-        $user = User::factory()->create();
         $data = [
             'description' => 'revenue test',
             'amount' => 1,
             'date' => '2022-01-02'
         ];
 
-        $response = $this->actingAs($user)
-            ->json('POST', '/api/revenue', $data, $this->header);
-
-        $response->assertOk();
+        $this->actingAs($this->user)
+            ->json('POST', '/api/revenue', $data, $this->header)
+            ->assertOk();
     }
 
     public function test_should_fail_validation_when_creating_revenue_without_description()
     {
-        $user = User::factory()->create();
         $data = [
             'amount' => 1,
             'date' => '2022-01-02'
         ];
 
-        $response = $this->actingAs($user)
-            ->json('POST', '/api/revenue', $data, $this->header);
-
-        $response->assertStatus(422)
+        $this->actingAs($this->user)
+            ->json('POST', '/api/revenue', $data, $this->header)
+            ->assertStatus(422)
             ->assertJsonValidationErrors('description');
     }
 
     public function test_should_create_a_revenue_successfully()
     {
-        $user = User::factory()->create();
         $data = [
             'description' => 'revenue test 3',
             'amount' => 20,
             'date' => '2022-01-02',
         ];
 
-        $response = $this->actingAs($user)
-            ->json('POST', '/api/revenue', $data, $this->header);
+        $this->actingAs($this->user)
+            ->json('POST', '/api/revenue', $data, $this->header)
+            ->assertOk();
 
-        $response->assertOk();
         $this->assertDatabaseHas('revenues', [
             'id' => 1,
             'description' => 'revenue test 3',
